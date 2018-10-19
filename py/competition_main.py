fa874fa27ec033c19d938323f8c228df3fa9f9ad
Ubuntu <ubuntu@ip-10-0-0-205.ap-northeast-1.compute.internal>
2018-10-19 08:23:04 +0000

    some

--- py/competition_main.py
+++ py/competition_main.py
@@ -1,3 +1,7 @@
+win_path = f'../features/bureau/*'
+win_path = f'../features/4_winner/*'
+stack_name='add_nest'
+fname='bureau'
 #========================================================================
 # argv[1] : model_type 
 # argv[2] : learning_rate
@@ -19,7 +23,7 @@ try:
     early_stopping_rounds = int(sys.argv[3])
 except IndexError:
     early_stopping_rounds = 150
-num_iterations = 20000
+num_boost_round = 10000
 
 try:
     pred_type = int(sys.argv[4])
@@ -29,24 +33,23 @@ except IndexError:
 import numpy as np
 import pandas as pd
 import datetime
+import glob
 import os
 HOME = os.path.expanduser('~')
 
-sys.path.append(f'{HOME}/kaggle/github/model/')
-from params_lgbm import xgb_params_0814, main_params
+sys.path.append(f'{HOME}/kaggle/data_analysis/model/')
+from params_lgbm import xgb_params_0814, params_home_credit
 
-sys.path.append(f"{HOME}/kaggle/github/library/")
+sys.path.append(f"{HOME}/kaggle/data_analysis/library/")
 import utils
 from make_file import make_feature_set
-from make_submission import make_submission
+from submission import make_submission
 from utils import logger_func
 logger=logger_func()
 
 if model_type=='lgb':
-    params = main_params()
+    params = params_home_credit()
     params['learning_rate'] = learning_rate
-    params['num_iterations'] = num_iterations
-    params['early_stopping_rounds'] = early_stopping_rounds
 elif model_type=='xgb':
     params = xgb_params_0814()
     params['eta'] = learning_rate
@@ -62,67 +65,89 @@ ignore_list = [key, 'SK_ID_BUREAU', 'SK_ID_PREV', target]
 
 def main():
 
-    pickle_name = 'add_clean_bureau'
-    pickle_name = 'add_clean_pre'
-    #  pickle_name = 'add_clean_ccb'
-    #  pickle_name = 'add_clean_ins'
-    #  pickle_name = 'add_clean_pos'
-    #  pickle_name = sys.argv[1]
-
-    base = pd.read_csv('../input/base.csv')[[key, target]]
-    win_path = f'../features/4_winner/*'
+    #  base = pd.read_csv('../input/base.csv')[[key, target]]
 
     #========================================================================
     # Data Load
     #========================================================================
     win_path_list = glob.glob(win_path)
-    win_feature_list = utils.pararell_load_data(path_list=win_path_list, delimiter='gz')
-    train = pd.concat(win_feature_list, axis=1)
-    #  base_train = utils.read_pkl_gzip('../input/base_train.gz')
-    #  train = pd.concat([base_train, train], axis=1)
+    train_path_list = []
+    test_path_list = []
+    for path in win_path_list:
+        if path.count('train'):
+            train_path_list.append(path)
+        elif path.count('test'):
+            test_path_list.append(path)
 
-    data.set_index(key, inplace=True)
-    use_cols = [col for col in data.columns if col not in ignore_list]
+    train_feature_list = utils.pararell_load_data(path_list=train_path_list, delimiter='gz')
+    test_feature_list = utils.pararell_load_data(path_list=test_path_list, delimiter='gz')
+    train = pd.concat(train_feature_list, axis=1)
+    test = pd.concat(test_feature_list, axis=1)
+
+    train.set_index(key, inplace=True)
+    test.set_index(key, inplace=True)
+    use_cols = [col for col in train.columns if col not in ignore_list]
     feature_num = len(use_cols)
 
-    stack_name=''
-    stack_name='add_nest'
+    metric = 'auc'
+    judge_flg = False
     fold=5
-    fold_type='group'
     fold_type='stratified'
-    dummie=0
+    group_col_name=''
+    dummie=1
     seed_num=1
+    train_args = {
+        'logger' : logger,
+        'train' : train,
+        'key' : key,
+        'target' : target,
+        'fold_type' : fold_type,
+        'fold' : fold,
+        'group_col_name' : group_col_name,
+        'params' : params,
+        'early_stopping_rounds' : early_stopping_rounds,
+        'num_boost_round' : num_boost_round,
+        'metric' : metric,
+        'judge_flg' : judge_flg,
+        'model_type' : model_type,
+        'dummie' : dummie,
+        'ignore_list' : ignore_list
+    }
 
-    score, result, result_stack = make_submission(
-        logger=logger
-        ,data=data
-        ,key=key
-        ,target=target
-        ,fold=fold
-        ,fold_type=fold_type
-        ,params=params
-        ,model_type=model_type
-        ,dummie=dummie
-        ,seed_num=seed_num
-        ,pred_type=1
-        ,ignore_list=ignore_list
-        ,stack_name=stack_name
-    )
-
+    #========================================================================
+    # Variables
+    #========================================================================
+    train_args['test'] = test
+    train_args['seed_num'] = seed_num
     submit = pd.read_csv('../input/sample_submission.csv')
-    if len(submit)>0:
-        submit[target] = result
-        submit.to_csv(f'../submit/{start_time[:12]}_submit_{model_type}_rate{learning_rate}_{feature_num}features_CV{score}_LB_early{early_stopping_rounds}_iter{num_iterations}.csv', index=False)
+    #  submit = []
+
+    #========================================================================
+    # Train & Prediction Start
+    #========================================================================
+    result, score, result_stack, use_cols = make_submission(
+        params = train_args
+    )
     #========================================================================
     # STACKING
     #========================================================================
     if len(stack_name)>0:
-        #  result_stack = base[key].to_frame().merge(result_stack, on=key, how='inner')
-        utils.to_pickle(path=f"../stack/{start_time[:12]}_{stack_name}_{model_type}_CV{str(score).replace('.', '-')}_{feature_num}features.fp", obj=result_stack)
-        #  result_stack.to_csv(f'../output/{start_time[:12]}_stack_{model_type}_rate{learning_rate}_{feature_num}features_CV{score_avg}_LB_early{early_stopping_rounds}_iter{num_iterations}.csv', index=False)
         logger.info(f'result_stack shape: {result_stack.shape}')
-    logger.info(f'FEATURE IMPORTANCE: {HOME}/kaggle/home-credit-default-risk/output/cv_feature{feature_num}_importances_auc_{score}.csv')
+        utils.to_pkl(path=f"../stack/{start_time[4:12]}_{stack_name}_{model_type}_CV{str(score).replace('.', '-')}_{feature_num}features.fp", obj=result_stack)
+    logger.info(f'FEATURE IMPORTANCE PATH: {HOME}/kaggle/home-credit-default-risk/output/cv_feature{feature_num}_importances_auc_{score}.csv')
 
+    #========================================================================
+    # Submission
+    #========================================================================
+    if len(submit)>0:
+        if stack_name=='add_nest':
+            test[target] = result
+            submit[key].to_frame().merge(test.reset_index()[[key, target]], on=key, how='left')
+            submit[target].fillna(0, inplace=True)
+            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{fname}_{model_type}_rate{learning_rate}_{feature_num}features_CV{score}_LB.csv', index=False)
+        else:
+            submit[target] = result
+            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{model_type}_rate{learning_rate}_{feature_num}features_CV{score}_LB.csv', index=False)
 
 if __name__ == '__main__':
     main()
--- py/select_feature.py
+++ py/select_feature.py
@@ -1,93 +1,186 @@
-code=0
+#========================================================================
+# argv[1]: code
+# argv[2]: valid_path
+# argv[3]: rank
+# argv[4]: session / user
+#========================================================================
+import sys
+try:
+    code = int(sys.argv[1])
+except IndexError:
+    code=0
+except ValueError:
+    pass
+try:
+    if sys.argv[4]=='one':
+        win_path = f'../features/4_winner/'
+        tmp_win_path = f'../features/5_tmp/'
+        test_path = f'../features/6_test_feat/'
+        tmp_test_path = f'../features/7_tmp_test/'
+        delete_train_path = f'../features/9_delete/'
+        delete_test_path = '../features/8_delete_test/'
+        second_path = '../features/2_second_valid/'
+    elif sys.argv[4]=='two':
+        win_path = f'../features/12_user_train/'
+        tmp_win_path = f'../features/10_user_tmp_train/'
+        test_path = f'../features/13_user_test/'
+        tmp_test_path = f'../features/11_user_tmp_test/'
+        delete_train_path = f'../features/14_user_train_delete/'
+        delete_test_path = f'../features/15_user_test_delete/'
+        second_path = '../features/10_user_tmp_train/'
+    else:
+        win_path = f'../features/4_winner/'
+        tmp_win_path = f'../features/5_tmp/'
+        test_path = f'../features/6_test_feat/'
+        tmp_test_path = f'../features/7_tmp_test/'
+        delete_train_path = f'../features/9_delete/'
+        delete_test_path = '../features/8_delete_test/'
+        second_path = '../features/2_second_valid/'
+except IndexError:
+    win_path = f'../features/4_winner/'
+    tmp_win_path = f'../features/5_tmp/'
+    test_path = f'../features/6_test_feat/'
+    tmp_test_path = f'../features/7_tmp_test/'
+    delete_train_path = f'../features/9_delete/'
+    delete_test_path = '../features/8_delete_test/'
+    second_path = '../features/2_second_valid/'
 #  code=1
 #  code=2
+import numpy as np
 import pandas as pd
 import os
 import shutil
-import sys
 import glob
 import re
+HOME = os.path.expanduser('~')
+sys.path.append(f"{HOME}/kaggle/data_analysis/library/")
+import utils
+from utils import logger_func
 
-unique_id = 'SK_ID_CURR'
-p_id = 'SK_ID_CURR'
-target = 'TARGET'
-ignore_features = [unique_id, target, 'valid_no', 'valid_no_4', 'is_train', 'is_test', 'SK_ID_PREV']
+
+#========================================================================
+# Global Variable
+from info_home_credit import hcdr_key_cols
+key, target, ignore_list = hcdr_key_cols()
+#========================================================================
 
 
-def move_to_second_valid(best_select=[], rank=0, key_list=[]):
+def to_win_dir_Nfeatures(path='../features/1_first_valid/*.gz', N=100):
+    path_list = glob.glob(path)
+    np.random.seed(1208)
+    np.random.shuffle(path_list)
+    path_list = path_list[:N]
+    for path in path_list:
+        try:
+            shutil.move('train_'+path, win_path)
+            shutil.move('test_'+path, win_path)
+        except shutil.Error:
+            shutil.move('train_'+path, '../features/9_delete')
+            shutil.move('test_'+path, '../features/9_delete')
 
+def move_to_second_valid(best_select=[], path='', rank=0, key_list=[]):
+    logger = logger_func()
     if len(best_select)==0:
+        try:
+            if path=='':
+                path = sys.argv[2]
+        except IndexError:
+            pass
+        best_select = pd.read_csv(path)
+        try:
+            if rank==0:
+                rank = int(sys.argv[3])
+        except IndexError:
+            pass
+        best_feature = best_select.query(f"rank>={rank}")['feature'].values
+        try:
+            best_feature = [col for col in best_feature if col.count(sys.argv[5])]
+        except IndexError:
+            best_feature = [col for col in best_feature if col.count('')]
         #  best_select = pd.read_csv('../output/use_feature/feature869_importance_auc0.806809193200456.csv')
-        best_select = pd.read_csv('../output/cv_feature1912_importances_auc_0.7940528815911253.csv')
-        #  best_select = pd.read_csv('../output/cv_feature1330_importances_auc_0.8066523340763816.csv')
-        #  best_select = pd.read_csv('../output/cv_feature1099_importances_auc_0.8072030486159842.csv')
         #  best_feature = best_select['feature'].values
         #  best_feature = best_select.query("rank>=750")['feature'].values
-        #  best_feature = best_select.query("rank>=100")['feature'].values
-        #  best_feature = best_select.query("rank>=2000")['feature'].values
-        best_feature = best_select.query("rank>=1000")['feature'].values
         #  best_feature = best_select.query("rank>=1047")['feature'].values
-        best_feature = [col for col in best_feature if col.count('max') or col.count('min') or col.count('sum')]
+        #  best_feature = [col for col in best_feature if col.count('max') or col.count('min') or col.count('sum')]
         #  best_feature = [col for col in best_feature if col.count('impute')]
 
         if len(best_feature)==0:
             sys.exit()
         for feature in best_feature:
-            if feature not in ignore_features:
+            if feature not in ignore_list:
                 try:
-                    shutil.move(f'../features/4_winner/{feature}.fp.gz', '../features/2_second_valid/')
+                    shutil.move(f"{win_path}train_{feature}.gz", second_path)
+                    shutil.move(f"{win_path}test_{feature}.gz", second_path)
                     #  shutil.move(f'../features/go_dima/{feature}.npy', '../features/1_second_valid/')
                 except FileNotFoundError:
-                    print(f'FileNotFound. : {feature}.fp')
+                    print(f'FileNotFound. : {feature}.gz')
                     pass
+                except shutil.Error:
+                    logger.info(f'Shutil Error: {feature}')
         print(f'move to third_valid:{len(best_feature)}')
-
-    else:
-        tmp = best_select.query(f"rank>={rank}")['feature'].values
-        for key in key_list:
-            best_feature = [col for col in tmp if col.count(key)]
-
-            if len(best_feature)==0:
-                sys.exit()
-            for feature in best_feature:
-                if feature not in ignore_features:
-                    shutil.move(f'../features/3_winner/{feature}.npy', '../features/1_third_valid')
-            print(f'move to third_valid:{len(best_feature)}')
-
 
 def move_to_use():
 
-    #  best_select = pd.read_csv('../output/cv_feature1476_importances_auc_0.8091815613330919.csv')
-    best_select = pd.read_csv('../output/cv_feature2715_importances_auc_0.7939371168035297.csv')
-    #  best_select = pd.read_csv('../output/cv_feature1194_importances_auc_0.809452251037472.csv')
+    try:
+        path = sys.argv[2]
+    except IndexError:
+        path = ''
+    best_select = pd.read_csv(path)
     best_feature = best_select['feature'].values
-    #  best_feature = best_select.query('flg_2==0')['feature'].values
-    #  best_feature = best_select.query('flg==0')['feature'].values
 
-    #  path_list_imp = glob.glob('../features/3_winner/*.npy')
-    #  impute_list = []
-    #  for path in path_list_imp:
-    #      imp_name = re.search(r'/([^/.]*).npy', path).group(1)[:-7]
-    #      impute_list.append(imp_name)
-    #  best_feature = dima_list
+    win_list = glob.glob(win_path + '*.gz')
+    for path in win_list:
+        filename = re.search(r'/([^/.]*).gz', path).group(1)
+        if filename  in ignore_list: continue
+        try:
+            shutil.move(path, tmp_win_path)
+        except shutil.Error:
+            shutil.move(path, delete_train_path)
 
-    path_list = glob.glob('../features/2_second_valid/*.fp.gz')
-    #  path_list = glob.glob('../features/3_third_valid/*.fp')
+    if sys.argv[4]=='one':
+        first_list = glob.glob('../features/1_first_valid/*.gz')
+        second_list = glob.glob('../features/2_second_valid/*.gz')
+        third_list = glob.glob('../features/3_third_valid/*.gz')
+        tmp_list = glob.glob('../features/5_tmp/*.gz')
+        path_list = first_list + second_list + third_list + tmp_list
+    elif sys.argv[4]=='two':
+        path_list = glob.glob(tmp_win_path + '*.gz')
+    else :
+        first_list = glob.glob('../features/1_first_valid/*.gz')
+        second_list = glob.glob('../features/2_second_valid/*.gz')
+        third_list = glob.glob('../features/3_third_valid/*.gz')
+        tmp_list = glob.glob('../features/5_tmp/*.gz')
+        path_list = first_list + second_list + third_list + tmp_list
 
+    done_list = []
     for path in path_list:
-        filename = re.search(r'/([^/.]*).fp.gz', path).group(1)
+
+        try:
+            filename = re.search(r'/([^/.]*).gz', path).group(1)
+        except AttributeError:
+            print(f"AttributeError: \nPathName: {path}")
+            sys.exit()
         if filename in best_feature:
-            shutil.move(path, '../features/4_winner/')
+            try:
+                shutil.move(path, win_path)
+                done_list.append(filename)
+            except shutil.Error:
+                shutil.move(path, delete_train_path)
+
+    logger = logger_func()
+    loss_list = set(list(best_feature)) - set(done_list)
+    logger.info(f"Loss List:")
+    for loss in loss_list:
+        logger.info(f"{loss}")
 
 
 def move_feature(feature_name, move_path='../features/9_delete'):
 
     try:
-        shutil.move(f'../features/4_winner/{feature_name}.fp.gz', move_path)
+        shutil.move(f'../features/4_winner/{feature_name}.gz', move_path)
     except FileNotFoundError:
-        print(f'FileNotFound. : {feature_name}.fp.gz')
+        print(f'FileNotFound. : {feature_name}.gz')
         pass
-
 
 
 def main():
@@ -97,6 +190,8 @@ def main():
         move_to_use()
     elif code==2:
         move_file()
+    elif code==4:
+        to_win_dir_Nfeatures(N=int(sys.argv[2]))
 
 
 if __name__ == '__main__':
