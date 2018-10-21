482a205bc73d8e99906e7f5667f098a1b58eb68d
Go Horikoshi <go.horikoshi@gixo.jp>
2018-10-21 10:27:02 +0900

    some

--- py/tmp_home_credit_main.py
+++ py/tmp_home_credit_main.py
@@ -39,11 +39,11 @@ HOME = os.path.expanduser('~')
 
 sys.path.append(f'{HOME}/kaggle/data_analysis/model/')
 from params_lgbm import xgb_params_0814, params_home_credit
-from lightgbm_ex import lightgbm_ex as lgb_ex
 
 sys.path.append(f"{HOME}/kaggle/data_analysis/library/")
 import utils
 from make_file import make_feature_set
+from submission import make_submission
 from utils import logger_func
 logger=logger_func()
 
@@ -84,51 +84,57 @@ def main():
     train = pd.concat(train_feature_list, axis=1)
     test = pd.concat(test_feature_list, axis=1)
 
+    train.set_index(key, inplace=True)
+    test.set_index(key, inplace=True)
+    use_cols = [col for col in train.columns if col not in ignore_list]
+    feature_num = len(use_cols)
+
     metric = 'auc'
+    judge_flg = False
     fold=5
     fold_type='stratified'
     group_col_name=''
     dummie=1
-    oof_flg=True
-    LGBM = lgb_ex(logger=logger, metric=metric, model_type=model_type, ignore_list=ignore_list)
-
-    train, _ = LGBM.data_check(df=train)
-    test, drop_list = LGBM.data_check(df=test, test_flg=True)
-    if len(drop_list):
-        train.drop(drop_list, axis=1, inplace=True)
-        test.drop(drop_list, axis=1, inplace=True)
+    seed_num=1
+    train_args = {
+        'logger' : logger,
+        'train' : train,
+        'key' : key,
+        'target' : target,
+        'fold_type' : fold_type,
+        'fold' : fold,
+        'group_col_name' : group_col_name,
+        'params' : params,
+        'early_stopping_rounds' : early_stopping_rounds,
+        'num_boost_round' : num_boost_round,
+        'metric' : metric,
+        'judge_flg' : judge_flg,
+        'model_type' : model_type+'_'+fname,
+        'dummie' : dummie,
+        'ignore_list' : ignore_list
+    }
 
     #========================================================================
-    # Train & Prediction Start
+    # Variables
     #========================================================================
-    LGBM.cross_prediction(
-        train=train
-        ,test=test
-        ,key=key
-        ,target=target
-        ,fold_type=fold_type
-        ,fold=fold
-        ,group_col_name=group_col_name
-        ,params=params
-        ,num_boost_round=num_boost_round
-        ,early_stopping_rounds=early_stopping_rounds
-        ,oof_flg=oof_flg
-    )
-    cv_score = LGBM.cv_score
-    result = LGBM.prediction
-    cv_feim = LGBM.cv_feim
-    feature_num = len(LGBM.use_cols)
-
+    train_args['test'] = test
+    train_args['seed_num'] = seed_num
     submit = pd.read_csv('../input/sample_submission.csv')
     #  submit = []
 
     #========================================================================
+    # Train & Prediction Start
+    #========================================================================
+    result, score, result_stack, use_cols = make_submission(
+        params = train_args
+    )
+    #========================================================================
     # STACKING
     #========================================================================
     if len(stack_name)>0:
-        logger.info(f'result_stack shape: {LGBM.result_stack.shape}')
-        utils.to_pkl(path=f"../stack/{start_time[4:12]}_{stack_name}_{model_type}_CV{str(cv_score).replace('.', '-')}_{feature_num}features.fp", obj=LGBM.result_stack)
-    logger.info(f'FEATURE IMPORTANCE PATH: {HOME}/kaggle/home-credit-default-risk/output/cv_feature{feature_num}_importances_auc_{cv_score}.csv')
+        logger.info(f'result_stack shape: {result_stack.shape}')
+        utils.to_pkl(path=f"../stack/{start_time[4:12]}_{stack_name}_{model_type}_CV{str(score).replace('.', '-')}_{feature_num}features.fp", obj=result_stack)
+    logger.info(f'FEATURE IMPORTANCE PATH: {HOME}/kaggle/home-credit-default-risk/output/cv_feature{feature_num}_importances_auc_{score}.csv')
 
     #========================================================================
     # Submission
@@ -139,10 +145,10 @@ def main():
             test = test.reset_index()[[key, target]].groupby(key)[target].mean().reset_index()
             submit = submit[key].to_frame().merge(test, on=key, how='left')
             submit[target].fillna(0, inplace=True)
-            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{fname}_{model_type}_rate{learning_rate}_{feature_num}features_CV{cv_score}_LB.csv', index=False)
+            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{fname}_{model_type}_rate{learning_rate}_{feature_num}features_CV{score}_LB.csv', index=False)
         else:
             submit[target] = result
-            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{model_type}_rate{learning_rate}_{feature_num}features_CV{cv_score}_LB.csv', index=False)
+            submit.to_csv(f'../submit/{start_time[4:12]}_submit_{model_type}_rate{learning_rate}_{feature_num}features_CV{score}_LB.csv', index=False)
 
 if __name__ == '__main__':
     main()
--- scp_py.py
+++ scp_py.py
@@ -6,4 +6,4 @@ path_list = glob.glob(f'{sys.argv[1]}')
 for path in path_list:
     #  print(path)
     #  os.system(f"scp -i /home/yryrgogo/gixo-hagihara.pem {path} ubuntu@13.231.227.78':/home/ubuntu/")
-    os.system(f"scp -i /home/yryrgogo/gixo-hagihara.pem {path} ubuntu@13.114.228.59:/home/ubuntu/kaggle/home-credit-default-risk{sys.argv[2]}")
+    os.system(f"scp -i /home/yryrgogo/gixo-hagihara.pem {path} ubuntu@52.193.163.244:/home/ubuntu/kaggle/home-credit-default-risk{sys.argv[2]}")
