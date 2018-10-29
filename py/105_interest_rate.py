b0cec2961502b132c30a49f9faa9eb8a081806fb
Ubuntu <ubuntu@ip-10-0-0-205.ap-northeast-1.compute.internal>
2018-10-29 17:15:31 +0900

    some

--- py/105_interest_rate.py
+++ py/105_interest_rate.py
@@ -1,3 +1,4 @@
+standard=True
 " Interest_Rate "
 import gc
 import numpy as np
@@ -27,6 +28,9 @@ cpy = 'CNT_PAYMENT'
 co_type = 'NAME_CONTRACT_TYPE'
 dd = 'DAYS_DECISION'
 
+#========================================================================
+# Previous ApplicationのInterest Rateを計算
+#========================================================================
 prev_ir = False
 if prev_ir:
     app = utils.read_df_pkl('../input/clean_app*')[[key, target]]
@@ -37,46 +41,106 @@ if prev_ir:
     # 金利合計（分割数考慮前）
     tmp_ir = (df[aan].values * df[cpy].values) / df[acr].values
 
-    for cnt in range(3, 91, 3):
+    for cnt in range(3, 61, 3):
         ir = ( tmp_ir * (cnt / df[cpy]) ) - 1.0
         df[f'ir_{cnt}'] = ir
-        df[f'ir_{cnt}'] = df[f'ir_{cnt}'].map(lambda x: x if (0.08<x) and (x<0.25) else np.nan)
+        df[f'ir_{cnt}'] = df[f'ir_{cnt}'].map(lambda x: x if (0.0<x) and (x<0.5) else np.nan)
         print(f"{cnt} :", len(df[f'ir_{cnt}'].dropna()))
         if len(df[f'ir_{cnt}'].dropna())<len(df)*0.001:
             df.drop(f'ir_{cnt}', axis=1, inplace=True)
 
     ir_cols = [col for col in df.columns if col.count('ir_')]
+    df['ir_mean'] = df[ir_cols].mean(axis=1)
     df['ir_max'] = df[ir_cols].max(axis=1)
     df['ir_min'] = df[ir_cols].min(axis=1)
     df['ir_std'] = df[ir_cols].std(axis=1)
     utils.to_df_pkl(df=df, path='../eda/', fname='1024_prev_ir')
 
 
+# Curren Applicationに対するCNT_PAYMENTの予測値
 df = utils.read_df_pkl('../input/clean_cpy*')
+df['Pred_CPY_diff_Cal_CPY@'] = df['CNT_PAYMENT'].values - (df['AMT_CREDIT'].values / df['AMT_ANNUITY'].values)
 
 
+if standard:
+    #========================================================================
+    # SK_ID_BUREAUが
+    #========================================================================
+    kb = 'SK_ID_BUREAU'
+    bur = utils.read_df_pkl('../input/clean_bur*')[[key, kb]].groupby(key)[kb].max().reset_index()
+    df = df.reset_index().merge(bur, on=key, how='left')
+
+    # TrainでSK_ID_BUREAUをもつレコード
+    train =df[~df[target].isnull()]
+    df_not = train[~train[kb].isnull()]
+    bur_bin = pd.qcut(x=df_not[kb], q=10)
+    df_not['bur_bin'] = bur_bin
+
+    # TrainでSK_ID_BUREAUをもたないレコード
+    df_null = train[train[kb].isnull()]
+    df_null['bur_bin'] = 'train_no_bureau'
+
+    train = pd.concat([df_not, df_null], axis=0).sort_index()
+
+    # Testはまとめて標準化する
+    test =df[df[target].isnull()]
+    test['bur_bin'] = 'test'
+    df = pd.concat([train, test], axis=0).sort_index()
+
+#========================================================================
+# Current ApplicationのInterest Rateを計算
+#========================================================================
 # CNT_PAYMENT
-col = cpy
-train_file_path = f"../features/1_first_valid/train_{col}"
-test_file_path = f"../features/1_first_valid/test_{col}"
+train_file_path = f"../features/1_first_valid/train_{cpy}"
+test_file_path = f"../features/1_first_valid/test_{cpy}"
 
-utils.to_pkl_gzip(obj=df[~df[target].isnull()][cpy].values, path=train_file_path)
-utils.to_pkl_gzip(obj=df[df[target].isnull()][cpy].values, path=test_file_path)
-sys.exit()
+# Current Application CNT_PAYMENT Save as Feature
+#  utils.to_pkl_gzip(obj=df[~df[target].isnull()][cpy].values, path=train_file_path)
+#  utils.to_pkl_gzip(obj=df[df[target].isnull()][cpy].values, path=test_file_path)
+#  utils.to_pkl_gzip(obj=df[~df[target].isnull()][ 'Pred_CPY_diff_Cal_CPY@' ].values, path=train_file_path)
+#  utils.to_pkl_gzip(obj=df[df[target].isnull()][ 'Pred_CPY_diff_Cal_CPY@' ].values, path=test_file_path)
 
 # 金利合計（分割数考慮前）
 tmp_ir = (df[aan].values * df[cpy].values) / df[acr].values
 
+# 金利が何回分の支払いに対して発生しているか不明なので、3回刻みで一通り作る
 for cnt in range(3, 61, 3):
     ir = ( tmp_ir * (cnt / df[cpy]) ) - 1.0
     df[f'ir_{cnt}@'] = ir
-    df[f'ir_{cnt}@'] = df[f'ir_{cnt}@'].map(lambda x: x if (0.08<x) and (x<0.25) else np.nan)
+    df[f'ir_{cnt}@'] = df[f'ir_{cnt}@'].map(lambda x: x if (0.0<x) and (x<0.5) else np.nan)
     print(f"{cnt} :", len(df[f'ir_{cnt}@'].dropna()))
     if len(df[f'ir_{cnt}@'].dropna())<len(df)*0.001:
         df.drop(f'ir_{cnt}@', axis=1, inplace=True)
+        continue
+
+    #========================================================================
+    # 時系列で標準化
+    #========================================================================
+    if standard:
+        tmp_mean = df.groupby('bur_bin')[f'ir_{cnt}@'].mean().reset_index().rename(columns={f'ir_{cnt}@':'mean'})
+        tmp_std = df.groupby('bur_bin')[f'ir_{cnt}@'].std().reset_index().rename(columns={f'ir_{cnt}@':'std'})
+        df = df.merge(tmp_mean, on='bur_bin', how='inner')
+        df = df.merge(tmp_std, on='bur_bin', how='inner')
+        #  df[f'stan_ir_{cnt}@'] = df[[f'ir_{cnt}@', 'mean', 'std']].apply(lambda x: (x[0]-x[1]) / x[2], axis=1)
+        df[f'stan_ir_{cnt}@'] = (df[f'ir_{cnt}@'].values - df['mean'].values) / df['std'].values
+        df.drop([f'ir_{cnt}@', 'mean', 'std'], axis=1, inplace=True)
 
 ir_cols = [col for col in df.columns if col.count('ir_') or col.count('CNT_PAY')]
 
+if standard:
+    df['stan_ir_mean@'] = df[ir_cols].mean(axis=1)
+    df['stan_ir_std@'] = df[ir_cols].std(axis=1)
+    df['stan_ir_max@'] = df[ir_cols].max(axis=1)
+    df['stan_ir_min@'] = df[ir_cols].min(axis=1)
+else:
+    df['ir_mean@'] = df[ir_cols].mean(axis=1)
+    df['ir_std@'] = df[ir_cols].std(axis=1)
+    df['ir_max@'] = df[ir_cols].max(axis=1)
+    df['ir_min@'] = df[ir_cols].min(axis=1)
+
+
+ir_cols = [col for col in df.columns if col.count('ir_') or col.count('CNT_PAY')]
+# Feature Save
 for col in ir_cols:
     if not(col.count('@')) or col in ignore_list:
         continue
@@ -94,8 +158,4 @@ for col in ir_cols:
     # COMPLETE MAKE FEATURE : {train_file_path}
     #========================================================================''')
 
-sys.exit()
-df['ir_max'] = df[ir_cols].max(axis=1)
-df['ir_min'] = df[ir_cols].min(axis=1)
-df['ir_std'] = df[ir_cols].std(axis=1)
 #  utils.to_df_pkl(df=df, path='../eda/', fname='1024_prev_ir')
--- py/110_neighbor_category.py
+++ py/110_neighbor_category.py
@@ -1,4 +1,4 @@
-feat_no = '108_'
+feat_no = '110_'
 prefix = feat_no
 ext_feat=False
 import numpy as np
@@ -33,6 +33,29 @@ ignore_list = [key, target, 'SK_ID_BUREAU', 'SK_ID_PREV']
 base = utils.read_df_pkl(path='../input/base_app*')
 fname = 'app'
 prefix = feat_no + f'{fname}_'
-df = utils.read_df_pkl(path=f'../input/clean_{fname}*.p')
+df = utils.read_df_pkl(path=f'../input/clean_{fname}*.p')[[key, target, 'EXT_SOURCE_2']]
 
+train_ir = utils.read_pkl_gzip('../features/4_winner/train_stan_ir_mean@.gz')
+test_ir = utils.read_pkl_gzip('../features/4_winner/test_stan_ir_mean@.gz')
+ir_mean = np.hstack((train_ir, test_ir))
+df['stan_ir_mean@'] = ir_mean
+df['stan_ir_mean@'].fillna('ir_nan', inplace=True)
 
+num_split = 9
+df['EXT_bin'] = pd.qcut(x=df['EXT_SOURCE_2'], q=num_split)
+df['ir_bin'] = pd.qcut(x=df['stan_ir_mean@'], q=num_split)
+
+col = f'neighbor{num_split**2}@'
+df[col] = df[['EXT_bin','ir_bin']].apply(lambda x: str(x[0]) + '_' + str(x[1]) if str(x[0])!=str(np.nan) else 'ext_nan', axis=1)
+
+train_feat = df[df[target]>=0][col].values
+test_feat = df[df[target].isnull()][col].values
+train_file_path = f"../features/1_first_valid/train_{prefix}{col}"
+test_file_path = f"../features/1_first_valid/test_{prefix}{col}"
+utils.to_pkl_gzip(obj=train_feat, path=train_file_path)
+utils.to_pkl_gzip(obj=test_feat, path=test_file_path)
+
+logger.info(f'''
+#========================================================================
+# COMPLETE MAKE FEATURE : {train_file_path}
+#========================================================================''')
